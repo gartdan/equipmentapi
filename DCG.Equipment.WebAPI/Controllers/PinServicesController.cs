d93a30c569ce8a74e57ac1785e3aa95085c993d9
Dan Gartner <dgartner@live.com>
2020-04-28 15:01:50 -0500

    Added an App Insights call

--- DCG.Equipment.WebAPI/Controllers/PinServicesController.cs
+++ DCG.Equipment.WebAPI/Controllers/PinServicesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,9 +11,17 @@ namespace DCG.Equipment.WebAPI.Controllers
     [ApiController]
     public class PinServicesController : ControllerBase
     {
+        private TelemetryClient _telemetry;
+
+        public PinServicesController(TelemetryClient telemetry)
+        {
+            this._telemetry = telemetry;
+        }
+
         [HttpPost("pin/search")]
         public PartManifest PinSearch(string pin)
         {
+            this._telemetry.TrackEvent($"PinSearch Called - {pin}");
             var manifest = new PartManifest() { Pin = pin };
             manifest.ModelNumber = "6215R";
             manifest.MachineType = "Row Crop Tractors";
