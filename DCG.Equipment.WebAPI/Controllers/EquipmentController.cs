ba50fd76d3330d212793dac1dc9e80b615bf5ee9
Dan Gartner <dgartner@live.com>
2020-06-08 15:01:14 -0500

    added some app insights calls

--- DCG.Equipment.WebAPI/Controllers/EquipmentController.cs
+++ DCG.Equipment.WebAPI/Controllers/EquipmentController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,28 +13,42 @@ namespace DCG.Equipment.WebAPI.Controllers
     {
 
         private EquipmentService _equipmentService = new EquipmentService();
+        private TelemetryClient _telemetry;
+
+        public EquipmentController(TelemetryClient telemetry)
+        {
+            this._telemetry = telemetry;
+        }
+
 
         [HttpGet("equipment/hello")]
         public string Hello()
         {
+            this._telemetry.TrackEvent("Hello");
             return "Hello";
         }
 
         [HttpGet("equipment/nearestdealer")]
         public Dealer NearestDealer(string latitude = "41.881832", string longitude = "-87.623177", string range = "5m")
         {
+            this._telemetry.TrackEvent("NearestDealer");
+            this._telemetry.TrackTrace($"Searching for nearest dealer: {latitude}, {longitude}");
             return Dealer.GetRandomDealer();
         }
 
         [HttpPost("equipment/topseller")]
         public Equipment GetTopSeller(string dealerId)
         {
+            this._telemetry.TrackEvent("GetTopSeller", new Dictionary<string, string>() { { "GetTopSeller", dealerId } });
+            this._telemetry.TrackTrace($"Searching for Top Seller: {dealerId}");
             return GetEquipmentDetails(dealerId);
         }
 
         [HttpPost("equipment/bytype")]
         public IList<Equipment> GetEquipmentByType(string equipmentType)
         {
+            this._telemetry.TrackEvent("GetEquipmentByType", new Dictionary<string, string>() { { "EquipmentType", equipmentType } });
+            this._telemetry.TrackTrace($"GetEquipmentByType: {equipmentType}");
             return _equipmentService.GetEquipmentByType(equipmentType);
         }
 
@@ -52,6 +67,8 @@ namespace DCG.Equipment.WebAPI.Controllers
         {
             if (modelNumber == "-1")
                 throw new ApplicationException($"Invalid Model Number {0}");
+            this._telemetry.TrackEvent("GetSimilarMachine");
+            this._telemetry.TrackTrace($"GetSimilarMachine: {modelNumber}");
             return _equipmentService.GetRandomEquipment();
         }
     }
